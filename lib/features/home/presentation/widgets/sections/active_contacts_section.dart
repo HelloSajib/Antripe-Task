da638abac5673d1b19953d12a7e82e2df55d398f
developerSajib88 <developersajib88@gmail.com>
2026-03-02 15:32:38 +0600

    :sparkles:[feat][home]: Contacts data set into contact list ui.

--- lib/features/home/presentation/widgets/sections/active_contacts_section.dart
+++ lib/features/home/presentation/widgets/sections/active_contacts_section.dart
@@ -3,14 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_task/core/utils/extensions/null_empty_extension.dart';
 import 'package:flutter_task/core/utils/extensions/status_extension.dart';
-import 'package:flutter_task/core/utils/styles/app_text_styles.dart';
-import 'package:flutter_task/core/utils/ui_helpers/margins.dart';
-import 'package:flutter_task/core/utils/ui_helpers/paddings.dart';
-import 'package:flutter_task/core/utils/ui_helpers/radius.dart';
-import 'package:flutter_task/core/utils/ui_helpers/spacing.dart';
 import 'package:flutter_task/features/home/presentation/bloc/home_bloc.dart';
 import 'package:flutter_task/features/home/presentation/widgets/item_view/active_contact_item_view.dart';
-import 'package:flutter_task/widgets/network_image_widget.dart';
 
 class ActiveContactsSection extends StatelessWidget {
   const ActiveContactsSection({super.key});
@@ -21,20 +15,20 @@ class ActiveContactsSection extends StatelessWidget {
       height: 90.h,
       child: BlocBuilder<HomeBloc, HomeState>(
         builder: (context, state) {
-          if(state.status.isSuccess && state.contacts.isNotNullAndNotEmpty){
+          if (state.status.isSuccess && state.contacts.isNotNullAndNotEmpty) {
             return ListView.builder(
-                itemCount: state.contacts?.length,
-                scrollDirection: .horizontal,
-                itemBuilder: (context, index) {
-                  final contact = state.contacts![index];
-                  return ActiveContactItemView(
-                    imageUrl: contact.avatarUrl ?? "",
-                    name: contact.name ?? "N/A",
-                    isActive: contact.status == "active",
-                  );
-                }
+              itemCount: state.contacts?.length,
+              scrollDirection: .horizontal,
+              itemBuilder: (context, index) {
+                final contact = state.contacts![index];
+                return ActiveContactItemView(
+                  imageUrl: contact.avatarUrl ?? "",
+                  name: contact.name ?? "N/A",
+                  isActive: contact.status == "active",
+                );
+              },
             );
-          }else{
+          } else {
             return SizedBox.shrink();
           }
         },
