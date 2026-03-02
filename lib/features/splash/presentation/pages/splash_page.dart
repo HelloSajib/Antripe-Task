46680de4fb7b261c5a728db4c8b611b4ff55d3dc
developerSajib88 <developersajib88@gmail.com>
2026-03-02 15:33:34 +0600

    :sparkles:[feat][home]: Contacts data set into contact list ui.

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -38,69 +38,67 @@ class SplashPage extends HookWidget {
       listener: (context, state)=> checkingOnboard(state),
       child:  Scaffold(
         backgroundColor: AppColors.background,
-        body: SafeArea(
-          child: SizedBox(
-            width: 1.sw,
-            height: 1.sh,
-            child: Stack(
-              alignment: .center,
-              children: [
+        body: SizedBox(
+          width: 1.sw,
+          height: 1.sh,
+          child: Stack(
+            alignment: .center,
+            children: [
 
-                Positioned(
-                  top: -80,
-                  left: -120,
-                  child: Container(
-                    width: 500.w,
-                    height: 500.h,
-                    decoration: BoxDecoration(
-                        shape: .circle,
-                        boxShadow: [
-                          BoxShadow(
-                              color: AppColors.primary.withValues(alpha: 0.1),
-                              blurRadius: 60.r
-                          )
-                        ]
-                    ),
-                  ),
-                ),
-
-                Positioned(
-                  right: -30,
-                  top: -30,
-                  child: Container(
-                    width: 160.w,
-                    height: 160.h,
-                    decoration: BoxDecoration(
+              Positioned(
+                top: -80,
+                left: -120,
+                child: Container(
+                  width: 500.w,
+                  height: 500.h,
+                  decoration: BoxDecoration(
                       shape: .circle,
-                      color: AppColors.primary,
-                    ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: AppColors.primary.withValues(alpha: 0.1),
+                            blurRadius: 60.r
+                        )
+                      ]
                   ),
                 ),
+              ),
 
-                Positioned(
-                  top: 250,
-                  child: SvgPicture.asset(
-                    "assets/images/app_logo.svg",
-                    width: 171.w,
-                    height: 168.w,
+              Positioned(
+                right: -30,
+                top: -30,
+                child: Container(
+                  width: 160.w,
+                  height: 160.h,
+                  decoration: BoxDecoration(
+                    shape: .circle,
+                    color: AppColors.primary,
                   ),
                 ),
+              ),
 
-                Positioned(
-                  left: -120,
-                  bottom: -120,
-                  child: Container(
-                    width: 353.w,
-                    height: 353.h,
-                    decoration: BoxDecoration(
-                      shape: .circle,
-                      color: AppColors.primary,
-                    ),
+              Positioned(
+                top: 250,
+                child: SvgPicture.asset(
+                  "assets/images/app_logo.svg",
+                  width: 171.w,
+                  height: 168.w,
+                ),
+              ),
+
+              Positioned(
+                left: -120,
+                bottom: -120,
+                child: Container(
+                  width: 353.w,
+                  height: 353.h,
+                  decoration: BoxDecoration(
+                    shape: .circle,
+                    color: AppColors.primary,
                   ),
-                )
+                ),
+              )
 
-              ],
-            ),
+            ],
           ),
         ),
       )
